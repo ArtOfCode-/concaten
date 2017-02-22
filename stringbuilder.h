b017245f783f17fb4e4971cf56eee39ecd6cf1c7
Nic Hartley <nxh9052@rit.edu>
2017-02-22 08:01:20 -0500

    Now I know what I have to do next (broke build, but it's dev)

--- stringbuilder.h
+++ stringbuilder.h
@@ -6,12 +6,11 @@
 
 // TODO No pointers to objects; does bad things with `const` apparently
 struct StringBuilder;
-typedef struct StringBuilder *StringBuilder;
-StringBuilder sb_new(size_t);
-bool sb_append(StringBuilder, char);
-char *sb_as_string(StringBuilder);
-char *sb_free_copy(StringBuilder);
-size_t sb_size(StringBuilder);
-void sb_free(StringBuilder);
+struct StringBuilder sb_new(size_t);
+bool sb_append(struct StringBuilder, char);
+char *sb_as_string(struct StringBuilder);
+char *sb_free_copy(struct StringBuilder);
+size_t sb_size(struct StringBuilder);
+void sb_free(struct StringBuilder);
 
 #endif //CONCATEN_STRINGBUILDER_H
