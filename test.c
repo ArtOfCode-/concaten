623af8eac8980ac10f9f89ab6830a1672ca7198f
Nic Hartley <nxh9052@rit.edu>
2017-02-21 21:45:42 -0500

    updated test file so we can independently test bits and pieces of the actual code, but the random side tests don't get pushed as part of the real code.

--- test.c
+++ test.c
@@ -1,8 +1,35 @@
-#define _CRTDBG_MAP_ALLOC
 #include <stdlib.h>
-#include <crtdbg.h>
+#include <stdio.h>
+#include "stringbuilder.h"
+
+void dbg_print(StringBuilder sb) {
+    printf("%zu chars: %s\n", sb_size(sb), sb_as_string(sb));
+}
 
 int main() {
-    _CrtSetDbgFlag ( _CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF );
-    int *leaking = malloc(sizeof(int));
+    StringBuilder sb = sb_new(4);
+    sb_append(sb, 'a');
+    sb_append(sb, 'b');
+    sb_append(sb, 'c');
+    dbg_print(sb);
+    sb_append(sb, 'd');
+    dbg_print(sb);
+    sb_append(sb, 'e');
+    dbg_print(sb);
+    sb_append(sb, 'f');
+    sb_append(sb, 'g');
+    sb_append(sb, 'h');
+    dbg_print(sb);
+    sb_append(sb, 'a');
+    sb_append(sb, 'b');
+    sb_append(sb, 'c');
+    dbg_print(sb);
+    sb_append(sb, '1');
+    dbg_print(sb);
+    char *str = sb_free_copy(sb);
+    if (!str) {
+        puts("Error!");
+    } else {
+        puts(str);
+    }
 }
