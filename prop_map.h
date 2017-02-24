6d41c4ac96c0980bfe2debae616b9be6f8e73adc
Nic Hartley <nxh9052@rit.edu>
2017-02-24 12:42:59 -0500

    started work on PropMap (since it's the bases of Object, which is after this)

---
+++ prop_map.h
@@ -0,0 +1,18 @@
+#ifndef CONCATEN_PROP_MAP_H
+#define CONCATEN_PROP_MAP_H
+
+struct StrToValPair {
+    const char *key;
+    size_t key_len;
+    int val; // TODO once Object is written, use it
+};
+struct Bucket {
+    size_t size;
+    StrToValPair *items;
+};
+struct PropMap {
+    size_t bucket_count;
+    Bucket *buckets;
+};
+
+#endif //CONCATEN_PROP_MAP_H
