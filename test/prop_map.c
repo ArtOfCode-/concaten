3e4bb5d4c827cb597185f3edd0646da40a4d3ca2
Nic <newbiedoodle@gmail.com>
2017-02-26 16:55:25 -0500

    test everything!

--- test/prop_map.c
+++ test/prop_map.c
@@ -37,17 +37,24 @@ void test_prop_map() {
     pm_set(&pm, "crum", __LINE__);
     full_print(&pm);
     pm_set(&pm, "00000000", __LINE__);
+    printf("%d\n", pm_get(pm, "00000000"));
+    printf("%d\n", pm_get(pm, "z"));
     pm_set(&pm, "0000000012345678", __LINE__);
-    full_print(&pm);
     pm_set(&pm, "12345678", __LINE__);
     pm_set(&pm, "abc4567887654321", __LINE__);
-    full_print(&pm);
     pm_set(&pm, "ab345678", __LINE__);
     pm_set(&pm, "foobaric12345678", __LINE__);
-    full_print(&pm);
     pm_set(&pm, "a2345678", __LINE__);
     pm_set(&pm, "ich bin euer sch", __LINE__);
     full_print(&pm);
     pm_set(&pm, "I am a nice guy!", __LINE__);
     full_print(&pm);
+    printf("get z: %d\n", pm_get(pm, "z"));
+    printf("is_key z: %d\n", pm_is_key(pm, "z"));
+    printf("is_val z: %d\n", pm_is_value(pm, 36));
+    pm_remove(&pm, "z");
+    printf("get z: %d\n", pm_get(pm, "z"));
+    printf("is_key z: %d\n", pm_is_key(pm, "z"));
+    printf("is_val z: %d\n", pm_is_value(pm, 36));
+    pm_free(&pm);
 }
