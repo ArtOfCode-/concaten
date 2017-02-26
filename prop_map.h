e11fea3690e6a3464c19b6d5f93a3844bb8f7355
Nic Hartley <nxh9052@rit.edu>
2017-02-25 19:23:21 -0500

    start adding functions

--- prop_map.h
+++ prop_map.h
@@ -1,10 +1,14 @@
 #ifndef CONCATEN_PROP_MAP_H
 #define CONCATEN_PROP_MAP_H
 
+
+#define PROP_MAP_VAL int
+#define PROP_MAP_INVALID -1
+
 struct StrToValPair {
     const char *key;
     size_t key_len;
-    int val; // TODO once Object is written, use it
+    int val;
 };
 struct Bucket {
     size_t size;
@@ -14,6 +18,9 @@ struct PropMap {
     size_t bucket_count;
     Bucket *buckets;
 };
+
+struct PropMap pm_new();
+bool pm_add(struct PropMap *, char *name, )
 
 // TODO finish this
 
