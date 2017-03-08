06f41ada5d31aceca5168349ca8693def7b1f7f5
Nic Hartley <nxh9052@rit.edu>
2017-03-07 19:43:09 -0500

    started on v0.2 (pick ds, define interface)

---
+++ data_stack.h
@@ -0,0 +1,26 @@
+#ifndef CONCATEN_DATA_STACK_H
+#define CONCATEN_DATA_STACK_H
+
+#include "object.h"
+
+// linked-list has O(1) push/pop always, and can be copied in O(1) (as long as it's not a deep copy!)
+
+struct DST_Node {
+    // we want the contents of _this object_, not necessarily the ones it points to,
+    // to be immutable, with the notable exception of the refcount.
+    struct DST_Node *const next;
+    struct Object *const value;
+    size_t refcount;
+};
+struct DataStack {
+    struct DST_Node *head;
+    size_t count;
+};
+
+struct DataStack dst_new();
+bool dst_push(struct DataStack *, Object *);
+struct Object *dst_pop(struct DataStack *);
+struct DataStack dst_copy(const struct DataStack);
+void dst_free(struct DataStack *);
+
+#endif //CONCATEN_DATA_STACK_H
