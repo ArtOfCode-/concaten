ce0d3b4fdc17e546024754f15359fb268ffc0fca
Nic Hartley <nxh9052@rit.edu>
2017-03-09 03:15:47 -0500

    partial impl. turns out thinking is hard at 0300.

--- token_stack.c
+++ token_stack.c
@@ -1,19 +1,44 @@
 #include "token_stack.h"
 
-struct TokenStack tst_new(const struct Tokenizer underlying) {
-    return (struct TokenStack) {
-            .head = NULL,
-            .tknr = underlying
-    };
-}
-
-struct TokenStack tst_copy(const struct TokenStack copying) {
-    return (struct TokenStack) {
-            .head = copying.head,
-            .tknr = tknr_
+void tst_token_node_free(struct TS_TokenNode *freeing) {
+    if (freeing) {
+        --freeing->refcount;
+        if (!freeing->refcount) {
+            tst_token_node_free(freeing->next);
+            free(freeing);
+        }
     }
 }
 
-bool tst_push(struct TokenStack *to, struct Token pushing) {
+void tst_layer_node_free(struct TS_LevelNode *freeing) {
+    if (freeing) {
+        --freeing->refcount;
+        if (!freeing->refcount) {
+            tst_token_node_free(freeing->token_head);
+            tst_layer_node_free(freeing->next);
+            free(freeing);
+        }
+    }
+}
+
+struct TokenStack tst_new(const struct Tokenizer underlying) {
+    struct TokenStack ret = (struct TokenStack) {
+            .level_head = malloc(sizeof(*ret.level_head)),
+            .tknr = underlying,
+            .tracking_changes = false,
+            .latest_change = NULL
+    };
+    *ret.level_head = (struct TS_LevelNode) {
+            .token_head = NULL,
+            .next = NULL,
+            .refcount = 1 // claimed by stack
+    };
+    return ret;
+}
+
+bool tst_push(struct TokenStack *to, const struct Token pushing) {
+    struct TS_LevelNode *level = to->level_head;
+    struct TS_TokenNode *old_head = level->token_head;
+    level->token_head = malloc(sizeof(*old_head));
     
 }
--- token_stack.h
+++ token_stack.h
@@ -5,26 +5,38 @@
 
 #include "tokenizer.h"
 
-struct TS_InnerNode {
-    struct TS_InnerNode *next;
+struct TS_TokenNode {
+    struct TS_TokenNode *next;
     struct Token value;
     size_t refcount;
 };
 
-struct TS_Node {
-    struct TS_Node *next;
-    struct TS_InnerNode *inner_head;
+struct TS_LevelNode {
+    struct TS_LevelNode *next;
+    struct TS_TokenNode *token_head;
     size_t refcount;
 };
 
+enum TSCN_Type { TSCN_TOKEN_POP, TSCN_TOKEN_PUSH, TSCN_LAYER_PUSH, TSCN_LAYER_POP };
+
+struct TS_ChangeNode {
+    struct TS_ChangeNode *prev;
+    size_t prev_count;
+    enum TSCN_Type type;
+    struct Token popped; // only set if type == TSNC_TOKEN_POP, otherwise empty
+};
+
 struct TokenStack {
-    struct TS_Node *head;
+    struct TS_LevelNode *level_head;
     struct Tokenizer tknr;
+    struct TS_ChangeNode *latest_change;
+    bool tracking_changes;
 };
 
 struct TokenStack tst_new(const struct Tokenizer);
-struct TokenStack tst_copy(const struct TokenStack);
-bool tst_push(struct TokenStack *, struct Token);
+void tst_save_state(struct TokenStack *);
+void tst_restore_state(struct TokenStack *);
+bool tst_push(struct TokenStack *, const struct Token);
 struct Token *tst_pop(struct TokenStack *);
 bool tst_push_layer(struct TokenStack *);
 bool tst_pop_layer(struct TokenStack *);
