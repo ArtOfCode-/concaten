31762ba2be86c288f8b411e130c498560a38f54c
Nic Hartley <nxh9052@rit.edu>
2017-03-10 22:57:06 -0500

    interface done I think

--- token_stack.h
+++ token_stack.h
@@ -42,6 +42,7 @@ void tst_save_state(struct TokenStack *);
 void tst_restore_state(struct TokenStack *);
 bool tst_push(struct TokenStack *, const struct Token);
 bool tst_pop(struct TokenStack *, struct Token *);
+bool tst_peek(const struct TokenStack, struct Token *);
 bool tst_push_level(struct TokenStack *);
 bool tst_pop_level(struct TokenStack *);
 void tst_free(struct TokenStack *);
