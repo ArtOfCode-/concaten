91e8467bb9184e624898c1d376623606ecdc4433
Nic Hartley <nxh9052@rit.edu>
2017-03-13 20:13:21 -0400

    more thoughts

--- main.c
+++ main.c
@@ -6,28 +6,40 @@ int main() {
     // in tests/
     puts("Hello, World!");
     // in pseudocode, though, this is what's going to happen:
-    /*
-     foreach (path in cli-args) {
-       tknr = tknr_from_file(path);
-       tokens = ts_new(tknr);
-       data = ds_new();
-       scopes = ss_new();
-       while (!tokens.empty()) {
-         operating_on = tokens.pop();
-         if (operating_on.is_word()) {
-           error = get_word(operating_on.value)(tokens, data, scopes);
-           if (error) {
-             printf("%s: %s\n", error.typename, error.message)
-             foreach (line in error.stacktrace) {
-               printf("  %s\n", line);
-             }
-             return error.code;
-           }
-         } else {
-           data.push(operating_on.value);
-         }
-         // TODO anything else here?
-       }
-     }
-     */
+  /*
+    // TODO Something to handle CLI args
+    foreach (path in cli-args) {
+      tknr = tknr_from_file(path);
+      tokens = ts_new(tknr);
+      data = ds_new();
+      scopes = ss_new();
+      while (!tokens.empty()) {
+        operating_on = tokens.pop();
+        if (operating_on.is_word()) {
+          candidates = scopes.get_words_with_name(operating_on.value);
+          save_stack_states();
+          foreach (c in candidates) {
+            error = c(tokens, data, scopes);
+            if (error) {
+              if (error.type == invalid_argument_type) {
+                restore_stack_states();
+                continue;
+              } else {
+                printf("%s: %s\n", error.type, error.message);
+                foreach (line in error.stacktrace) {
+                  printf("  %s\n", line);
+                }
+                return error.code;
+              }
+            } else {
+              break;
+            }
+          }
+        } else {
+          data.push(operating_on.value);
+        }
+      }
+    }
+    return 0;
+  */
 }
