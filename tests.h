bb79d828ad28d016fc9738115245e87f196efa9d
Nic Hartley <nxh9052@rit.edu>
2017-03-14 22:33:27 -0400

    added utility macros

--- tests.h
+++ tests.h
@@ -3,6 +3,18 @@
 
 #include <stdio.h>
 
+#define _str_back(x) #x
+#define str(x) _str_back(x)
+#define tassert(cond, ...) do {\
+    ++total; \
+    if (cond) { ++successes; } \
+    else { \
+        printf(__FILE__ ":" str(__LINE__) " test failed: "); \
+        printf(__VA_ARGS__); \
+        puts(""); \
+    } \
+} while(0)
+
 void test_tokenizer();
 void test_prop_map();
 void test_method_map();
