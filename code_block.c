4bbebcfc90b25b7df84ae2f4b78e63f9e0e20f6b
Nic Hartley <nxh9052@rit.edu>
2017-03-16 17:18:56 -0400

    untested impls for all of them.

--- code_block.c
+++ code_block.c
@@ -26,6 +26,60 @@ bool cb_copy(const struct CodeBlock from, struct CodeBlock *into) {
     return true;
 }
 
+bool cb_expand(struct CodeBlock *cb) {
+    size_t new_cap = cb->cap * 2 * sizeof(struct Token);
+    struct Token *new_mem = realloc(cb->tokens, new_cap);
+    if (!new_mem) return false;
+    cb->tokens = new_mem;
+    cb->cap *= 2;
+    return true;
+}
+
 bool cb_append(struct CodeBlock *to, const struct Token adding) {
-    return false;
+    if (to->count == to->cap && !cb_expand(to)) {
+        return false;
+    }
+    to->tokens[to->count++] = adding;
+    return true;
+}
+
+bool cb_insert(struct CodeBlock *into, size_t pos, struct Token what) {
+    if (into->count == into->cap && !cb_expand(into)) {
+        return false;
+    }
+    for (size_t i = into->count; i > pos; --i) {
+        into->tokens[i] = into->tokens[i - 1];
+    }
+    into->tokens[pos] = what;
+    ++into->count;
+    return true;
+}
+
+bool cb_remove(struct CodeBlock *from, size_t pos) {
+    for (size_t i = pos; i < from->count; ++i) {
+        from->tokens[i] = from->tokens[i + 1];
+    }
+    --from->count;
+    return true;
+}
+
+bool cb_prepend(struct CodeBlock *to, struct Token what) {
+    return cb_insert(to, 0, what);
+}
+
+bool cb_get(const struct CodeBlock from, size_t idx, struct Token *into) {
+    if (idx >= from.count) return false;
+    *into = from.tokens[idx];
+    return true;
+}
+
+bool cb_set(struct CodeBlock *in, size_t idx, struct Token new_val) {
+    if (idx >= in->count) return false;
+    in->tokens[idx] = new_val;
+    return true;
+}
+
+void cb_free(struct CodeBlock *freeing) {
+    free(freeing->tokens);
+    freeing->tokens = NULL;
 }
