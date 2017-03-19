695fa78a46d265924154c17076058f67e898c928
Nic Hartley <nxh9052@rit.edu>
2017-03-19 02:15:26 -0400

    moved to 8k so we can fit stringbuilder in

--- error.h
+++ error.h
@@ -39,35 +39,36 @@ static const ERROR NO_ERROR = 0;
 //extern const ERROR  PROP_MAP_FAIL;                //  5500
 //extern const ERROR  METHOD_MAP_FAIL;              //  5750
 //extern const ERROR  RUNNABLE_FAIL;                //  6000
-//extern const ERROR  TOKENIZER_FAIL;               //  7000
-//extern const ERROR   CTOR_FAIL;                   //   7100
-//extern const ERROR    CTOR_STR_FAIL;              //    7110
-  extern const ERROR     CTOR_STR_MALLOC_FAIL;      //     7111
-  extern const ERROR     CTOR_STR_BAD_STRLEN_FAIL;  //     7112
-  extern const ERROR     CTOR_STR_NULL_ARG_FAIL;    //     7113
-//extern const ERROR    CTOR_FILE_FAIL;             //    7120
-  extern const ERROR     CTOR_FILE_MALLOC_FAIL;     //     7121
-  extern const ERROR     CTOR_FILE_BAD_STRLEN_FAIL; //     7122
-  extern const ERROR     CTOR_FILE_NULL_ARG_FAIL;   //     7123
-  extern const ERROR     CTOR_FILE_FOPEN_FAIL;      //     7124
-//extern const ERROR   READ_CHAR_FAIL;              //   7200
-  extern const ERROR    FILE_READ_FAIL;             //    7210
-  extern const ERROR     FILE_READ_EOF_FAIL;        //     7211
-//extern const ERROR    STRING_READ_FAIL;           //    7220
-  extern const ERROR     STRING_READ_EOS_FAIL;      //     7221
-//extern const ERROR   NEXT_TOKEN_FAIL;             //   7300
-  extern const ERROR    NT_MALLOC_FAIL;             //    7301
-  extern const ERROR    NT_NEW_SB_FAIL;             //    7302
-  extern const ERROR    NT_SB_FREE_COPY_FAIL;       //    7303
-  extern const ERROR    NT_INPUT_END_FAIL;          //    7304
-//extern const ERROR  TOKENIZER_SYNTAX_FAIL;        //  7500
-  extern const ERROR   SYN_NO_SEPARATION_FAIL;      //   7501
-  extern const ERROR   SYN_UNEXPECTED_END_FAIL;     //   7502
-//extern const ERROR   SYN_STR_FAIL;                //   7510
-  extern const ERROR    SYN_STR_MULTILINE_FAIL;     //    7511
-//extern const ERROR   SYN_NUM_FAIL;                //   7520
-  extern const ERROR    SYN_NUM_ILLEGAL_DIGIT_FAIL; //    7521
-//extern const ERROR   SYN_RGX_FAIL;                //   7530
-  extern const ERROR    SYN_RGX_BAD_FLAG_FAIL;      //    7531
+//extern const ERROR  STRINGBUILDER_FAIL;           //  7000
+//extern const ERROR  TOKENIZER_FAIL;               //  8000
+//extern const ERROR   CTOR_FAIL;                   //   8100
+//extern const ERROR    CTOR_STR_FAIL;              //    8110
+  extern const ERROR     CTOR_STR_MALLOC_FAIL;      //     8111
+  extern const ERROR     CTOR_STR_BAD_STRLEN_FAIL;  //     8112
+  extern const ERROR     CTOR_STR_NULL_ARG_FAIL;    //     8113
+//extern const ERROR    CTOR_FILE_FAIL;             //    8120
+  extern const ERROR     CTOR_FILE_MALLOC_FAIL;     //     8121
+  extern const ERROR     CTOR_FILE_BAD_STRLEN_FAIL; //     8122
+  extern const ERROR     CTOR_FILE_NULL_ARG_FAIL;   //     8123
+  extern const ERROR     CTOR_FILE_FOPEN_FAIL;      //     8124
+//extern const ERROR   READ_CHAR_FAIL;              //   8200
+  extern const ERROR    FILE_READ_FAIL;             //    8210
+  extern const ERROR     FILE_READ_EOF_FAIL;        //     8211
+//extern const ERROR    STRING_READ_FAIL;           //    8220
+  extern const ERROR     STRING_READ_EOS_FAIL;      //     8221
+//extern const ERROR   NEXT_TOKEN_FAIL;             //   8300
+  extern const ERROR    NT_MALLOC_FAIL;             //    8301
+  extern const ERROR    NT_NEW_SB_FAIL;             //    8302
+  extern const ERROR    NT_SB_FREE_COPY_FAIL;       //    8303
+  extern const ERROR    NT_INPUT_END_FAIL;          //    8304
+//extern const ERROR  TOKENIZER_SYNTAX_FAIL;        //  8500
+  extern const ERROR   SYN_NO_SEPARATION_FAIL;      //   8501
+  extern const ERROR   SYN_UNEXPECTED_END_FAIL;     //   8502
+//extern const ERROR   SYN_STR_FAIL;                //   8510
+  extern const ERROR    SYN_STR_MULTILINE_FAIL;     //    8511
+//extern const ERROR   SYN_NUM_FAIL;                //   8520
+  extern const ERROR    SYN_NUM_ILLEGAL_DIGIT_FAIL; //    8521
+//extern const ERROR   SYN_RGX_FAIL;                //   8530
+  extern const ERROR    SYN_RGX_BAD_FLAG_FAIL;      //    8531
 
 #endif //CONCATEN_ERROR_H
