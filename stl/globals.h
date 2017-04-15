1e2e0f4c59b80a4bbe0c7d28024f2b4943e703ac
Nic Hartley <nxh9052@rit.edu>
2017-04-14 23:38:28 -0400

    OK, now they're all the same as the known-working string.h, and STILL BROKEN SOMEHOW
    I think GCC is just broken.

--- stl/globals.h
+++ stl/globals.h
@@ -2,7 +2,6 @@
 #define CONCATEN_INTEGRAL_H
 
 #include "../error.h"
-#include "../method_map.h"
 
 extern struct MethodMap *global_funcs;
 ERROR init_globals();
--- stl/integral.h
+++ stl/integral.h
@@ -2,7 +2,6 @@
 #define CONCATEN_INTEGRAL_H
 
 #include "../error.h"
-#include "../method_map.h"
 
 extern struct MethodMap *integral_methods;
 ERROR init_integral_methods();
